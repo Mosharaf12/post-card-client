47609eebc2a4f369cc6cb70bab97a5a5af44dfe9
unknown <mosharafctg111@gmail.com>
2022-12-27 15:08:27 +0600

    added the login and register

--- src/Login/Register.js
+++ src/Login/Register.js
@@ -2,9 +2,35 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
+        <div className="hero bg-white shadow-xl py-24 my-5 rounded-lg">
+        <div className="hero-content flex-col lg:flex-row-reverse gap-20">
+          <div className="text-center lg:text-left">
             
+            <img className='rounded-lg' src="https://media.tenor.com/p0G_bmA2vSYAAAAd/login.gif" alt="" srcset="" />
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm bg-base-100">
+            <div className="card-body">
+            <h1 className="text-5xl font-bold text-center">Register Now!</h1>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="text" placeholder="email" className="input input-bordered" />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="text" placeholder="password" className="input input-bordered" />
+              
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
